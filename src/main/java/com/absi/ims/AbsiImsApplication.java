d60a3dddd4ab04a5031822f0be9039307ecdd988
Phillipp Vincent Ensomo <vincent.ensomo@orangeandbronze.com>
2015-07-13 16:15:10 +0800

    initial project structure

---
+++ src/main/java/com/absi/ims/AbsiImsApplication.java
@@ -0,0 +1,12 @@
+package com.absi.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsiImsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsiImsApplication.class, args);
+    }
+}
