8413520b14547361c9c1b1b40d0724ede3947291
Phillipp Vincent Ensomo <vincent.ensomo@orangeandbronze.com>
2015-09-02 07:47:20 +0800

    completed CRUD functionality for IMSUser

--- src/main/java/com/absi/ims/domain/IMSUser.java
+++ src/main/java/com/absi/ims/domain/IMSUser.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "ims_user")
@@ -24,18 +26,32 @@ public class IMSUser extends Auditable implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO, generator = "IMSUserSequence")
 	private Long id;
 
+	@Size(min=1, max=35)
 	@Column(name = "first_name")
 	private String firstname;
 
+	@Size(min=1, max=35)
 	@Column(name = "middle_name")
 	private String middlename;
 
+	@Size(min=1, max=35)
 	@Column(name = "last_name")
 	private String lastname;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "gender")
 	private Gender gender;
+	
+	@Version
+	private Long version;
+	
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
 
 	public Long getId() {
 		return id;
